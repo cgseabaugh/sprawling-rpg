00c916c37ccf3b3c2eb387a54f36bac57757dae6
Matthew Maher <matt@studiolab.io>
2022-08-06 08:45:46 -0700

    update webpack config, only use ReactRefreshPlugin in development mode

--- webpack.config.js
+++ webpack.config.js
@@ -4,66 +4,70 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 
-module.exports = ({ mode } = { mode: "development" }) => {
-    console.log(`mode is: ${mode}`);
+module.exports = (env, argv) => {
+  console.log('env:');
+  console.dir(env);
 
-    return {
-            mode,
-            entry: "./src/index.js",
-            devServer: {
-                client: { 
-                    overlay: false,
-                    logging: 'warn' },
-            },
-            output: {
-                publicPath: "/",
-                path: path.resolve(__dirname, "build"),
-                filename: "bundle.js"
-            },
-            module: {
-                rules: [
-                    {
-                        test: /\.(js|jsx)$/,
-                        exclude: /node_modules/,
-                        use: "babel-loader"
-                    },
-                    // {
-                    //     test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
-                    //     exclude: /node_modules/,
-                    //     use: ["url-loader", "file-loader"],
-                    // },
-                    {
-                        test: /\.(gif|png|jpe?g)$/,
-                        use: [
-                          {
-                            loader: 'file-loader',
-                            options: {
-                              name: '[name].[ext]',
-                              outputPath: 'assets/images/'
-                            }
-                          }
-                        ]
-                      },
-                    {
-                        test: /\.sa?css$/,
-                        exclude: /node_modules/,
-                        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader", "sass-loader"]
-                    },
-                    {
-                        test: /\.css$/i,
-                        exclude: /node-modules/,
-                        use: [
-                            'style-loader',
-                            'css-loader'
-                        ]
-                    }
-                ]
-            },
-            plugins: [
-                new HtmlWebpackPlugin({template: "./public/index.html"}),
-                new ReactRefreshPlugin(),
-                new MiniCssExtractPlugin(),
-            ],
+  console.log('argv:');
+  console.dir(argv);
+
+  const plugins = [
+    new HtmlWebpackPlugin({template: "./public/index.html"}),
+    new MiniCssExtractPlugin(),
+  ]
+
+  if (env.mode === 'development') {
+    plugins.push(new ReactRefreshPlugin())
+  }
+
+  return {
+    mode: env.mode,
+    entry: "./src/index.js",
+    devServer: {
+      client: {
+        overlay: false,
+        logging: 'warn'
+      },
+    },
+    output: {
+      publicPath: "/",
+      path: path.resolve(__dirname, "build"),
+      filename: "bundle.js"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: "babel-loader"
+        },
+        {
+          test: /\.(gif|png|jpe?g)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'assets/images/'
+              }
+            }
+          ]
+        },
+        {
+          test: /\.sa?css$/,
+          exclude: /node_modules/,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader", "sass-loader"]
+        },
+        {
+          test: /\.css$/i,
+          exclude: /node-modules/,
+          use: [
+            'style-loader',
+            'css-loader'
+          ]
         }
-
+      ]
+    },
+    plugins: plugins,
+  }
 };
